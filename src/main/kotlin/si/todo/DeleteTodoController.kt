d99a8d5f13f014ded81a065af76ea07fdafabb7d
simon brahan <tntpiranha@gmail.com>
2020-03-03 20:56:07 +0000

    Added POST API method to delete todo.

---
+++ src/main/kotlin/si/todo/DeleteTodoController.kt
@@ -0,0 +1,26 @@
+package si.todo
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+import java.sql.*
+import java.util.Properties
+
+@RestController
+class DeleteTodoController {
+
+    @PostMapping("/delete")
+    fun createTodo(@RequestParam(value = "id") id: Long) {
+        val connectionProps = Properties()
+        connectionProps.put("user", "root")
+        connectionProps.put("password", "root")
+
+        Class.forName("com.mysql.jdbc.Driver").newInstance()
+        val conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/todos", connectionProps)
+
+        val stmnt = conn.prepareStatement("DELETE FROM todo WHERE id = ?")
+        stmnt.setLong(1, id)
+        stmnt.executeUpdate()
+    }
+}
