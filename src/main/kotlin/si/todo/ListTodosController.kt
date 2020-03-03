c0e8be52811e817d31e328beb45cce8fc70739b9
simon brahan <tntpiranha@gmail.com>
2020-03-03 20:13:37 +0000

    Endpoint '/get' returns todos from database.

--- src/main/kotlin/si/todo/ListTodosController.kt
+++ src/main/kotlin/si/todo/ListTodosController.kt
@@ -1,17 +1,32 @@
 package si.todo
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+import java.sql.*
+import java.util.Properties
 
 @RestController
 class ListTodosController {
 
     @GetMapping("/get")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-        arrayOf(
-            Todo(1, "Prepare Barbeque"),
-            Todo(2, "Hang Out Washing"),
-            Todo(3, "Grocery Shopping")
-        )
+    fun greeting(): List<Todo> {
+        val connectionProps = Properties()
+        connectionProps.put("user", "root")
+        connectionProps.put("password", "root")
+
+        Class.forName("com.mysql.jdbc.Driver").newInstance()
+        val conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/todos", connectionProps)
+
+        val stmnt = conn.createStatement();
+        val res = stmnt.executeQuery("SELECT id, content FROM todo;")
+
+        return res.use {
+            sequence {
+                while (res.next()) {
+                    yield(Todo(res.getLong("id"), res.getString("content")))
+                }
+            }.toList()
+        }
+    }
 }
